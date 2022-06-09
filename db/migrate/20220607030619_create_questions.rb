98df751de1c1c9cdee65f2d2ac396c3b4e663bd8
kyoko0522 <kyo08023971867@gmail.com>
2022-06-09 03:19:08 +0000

    create model

--- db/migrate/20220607030619_create_questions.rb
+++ db/migrate/20220607030619_create_questions.rb
@@ -1,6 +1,9 @@
 class CreateQuestions < ActiveRecord::Migration[5.2]
   def change
     create_table :questions do |t|
+        t.string :title
+        t.text :body
+        t.integer :user_id
 
       t.timestamps
     end
--- db/migrate/20220607030653_create_answers.rb
+++ db/migrate/20220607030653_create_answers.rb
@@ -1,6 +1,9 @@
 class CreateAnswers < ActiveRecord::Migration[5.2]
   def change
     create_table :answers do |t|
+      t.integer :user_id
+      t.integer :question_id
+      t.text :body
 
       t.timestamps
     end
